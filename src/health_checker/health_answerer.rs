289a083404a1665ba45344c81727d5729c16bd4c
manudelca <manueldelcarril@gmail.com>
2022-07-03 23:44:00 -0300

    Now health_answerer has graceful quit

--- src/health_checker/health_answerer.rs
+++ src/health_checker/health_answerer.rs
@@ -1,27 +1,28 @@
 use std::net::{Shutdown, TcpListener};
+use std::io;
+use std::io::Write;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::Duration;
 use crate::health_checker::health_base::HealthBase;
 use crate::health_checker::health_checker_handler::HealthCheckerHandler;
 use crate::health_checker::health_msg::HealthMsg;
 
 pub struct HealthAnswerer {
     listener: TcpListener,
-    shutdown: bool
+    shutdown: Arc<AtomicBool>
 }
 
 impl HealthAnswerer {
 
     #[allow(dead_code)]
-    pub fn new(address: &str) -> Self {
+    pub fn new(address: &str, shutdown: Arc<AtomicBool>) -> Self {
+        let listener = TcpListener::bind(address).expect("Bind failed in health answerer");
+        listener.set_nonblocking(true).expect("Could not set non blocking to true");
         HealthAnswerer {
-            listener: TcpListener::bind(address).expect("Bind failed in health answerer"),
-            shutdown: false
+            listener,
+            shutdown
         }
-    }
-
-    #[allow(dead_code)]
-    pub fn shutdown(&mut self) {
-        self.shutdown = true;
-        self.listener.set_nonblocking(true).expect("Could not set non blocking to true");
     }
 
 }
@@ -29,13 +30,22 @@ impl HealthAnswerer {
 impl HealthBase for HealthAnswerer {
     fn run(&self, handler: &mut dyn HealthCheckerHandler) {
         for stream in self.listener.incoming() {
-            if self.shutdown {
+            if self.shutdown.load(Ordering::Relaxed) {
                 break;
             }
-            println!("Received new client");
-            let stream = stream.expect("Failed to accept new connection in run_health_answerer");
-            self.answer_health_messages(&stream, HealthMsg::Ping, HealthMsg::Pong, handler);
-            let _ = stream.shutdown(Shutdown::Both);
+            match stream {
+                Ok(mut stream) => {
+                    println!("Received new client");
+                    self.answer_health_messages(&stream, HealthMsg::Ping, HealthMsg::Pong, handler);
+                    stream.write_all(&[HealthMsg::Exit as u8]).unwrap();
+                    let _ = stream.shutdown(Shutdown::Both);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    std::thread::sleep(Duration::from_secs(1));
+                }
+                Err(e) => panic!("Unknown error while receiving clients: {}", e),
+            }
+
         }
     }
 
