bfc551b6d6f47b27dd7ac89ac8735360c6e73fa4
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2022-07-07 04:00:05 -0300

    gracefully quit post producer

--- src/bin/post_producer.rs
+++ src/bin/post_producer.rs
@@ -1,21 +1,21 @@
-use std::net::TcpListener;
 use amiquip::{ExchangeType, Result};
 use envconfig::Envconfig;
 use log::info;
+use std::net::TcpListener;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+use tp2::health_checker::health_answerer::HealthAnswerer;
+use tp2::health_checker::health_answerer_handler::HealthAnswerHandler;
+use tp2::health_checker::health_base::HealthBase;
 use tp2::messages::Message;
 use tp2::middleware::buf_exchange::BufExchange;
 use tp2::middleware::connection::{BinaryExchange, RabbitConnection};
 use tp2::middleware::RabbitExchange;
 use tp2::post::PostIterator;
-use tp2::{Config, POSTS_SOURCE_EXCHANGE_NAME};
-use tp2::health_checker::health_answerer::HealthAnswerer;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
-use tp2::health_checker::health_base::HealthBase;
-use tp2::health_checker::health_answerer_handler::HealthAnswerHandler;
 use tp2::sigterm_handler::sigterm_handler::handle_sigterm;
-
+use tp2::{Config, POSTS_SOURCE_EXCHANGE_NAME};
 
 fn main() -> Result<()> {
     let env_config = Config::init_from_env().unwrap();
@@ -27,16 +27,17 @@ fn main() -> Result<()> {
     let sigterm_handler_join = thread::spawn(move || handle_sigterm(shutdown_handler_join));
     let health_answerer = HealthAnswerer::new("0.0.0.0:6789", shutdown.clone());
     let mut health_answerer_handler = HealthAnswerHandler::new(shutdown.clone());
-    let health_answerer_thread = thread::spawn(move || {health_answerer.run(&mut health_answerer_handler)});
-    while !shutdown.load(Ordering::Relaxed) {
-        run_service(env_config.clone())?;
-    }
+    let health_answerer_thread =
+        thread::spawn(move || health_answerer.run(&mut health_answerer_handler));
+    run_service(env_config.clone(), shutdown.clone())?;
     shutdown.store(true, Ordering::Relaxed);
-    health_answerer_thread.join().expect("Failed to join health_answerer_thread");
+    health_answerer_thread
+        .join()
+        .expect("Failed to join health_answerer_thread");
     Ok(())
 }
 
-fn run_service(config: Config) -> Result<()> {
+fn run_service(config: Config, shutdown: Arc<AtomicBool>) -> Result<()> {
     let connection = RabbitConnection::new(&config)?;
     {
         let consumers = str::parse::<usize>(&config.consumers).unwrap();
@@ -45,18 +46,37 @@ fn run_service(config: Config) -> Result<()> {
         let bin_exchange = BinaryExchange::new(exchange, None, 1, consumers);
         let mut exchange = BufExchange::new(bin_exchange);
         let listener = TcpListener::bind("0.0.0.0:9090").expect("Could not bind listener");
-        let (stream, _) = listener.accept().expect("Could not accept server connection");
-        let posts = PostIterator::from_stream(stream);
-        info!("Iterating posts");
-        let published = posts
-            .map(Message::FullPost)
-            .flat_map(|message| exchange.send(&message))
-            .count();
+        
+        listener
+            .set_nonblocking(true)
+            .expect("Could not set non blocking to true");
 
-        exchange.end_of_stream()?;
+        for stream_result in listener.incoming() {
+            match stream_result {
+                Ok(stream) => {
 
-        info!("Published {} posts", published);
-        info!("Exit");
+                    let posts = PostIterator::from_stream(stream);
+                    info!("Iterating posts");
+                    let published = posts
+                        .map(Message::FullPost)
+                        .flat_map(|message| exchange.send(&message))
+                        .count();
+
+                    exchange.end_of_stream()?;
+
+                    info!("Published {} posts", published);
+                    info!("Exit");
+                }
+                Err(_) => {
+                    if shutdown.load(Ordering::Relaxed) {
+                        break
+                    } else {
+                        thread::sleep(Duration::from_secs(1));
+                        continue
+                    }
+                }
+            }
+        }
     }
     connection.close()
 }
