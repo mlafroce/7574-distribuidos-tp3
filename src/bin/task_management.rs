e4c8221a92513a5b076abc1654b7a9c16a665df4
manudelca <manueldelcarril@gmail.com>
2022-07-07 22:54:54 -0300

    Removed HANDLE SIGTERM log

--- src/bin/task_management.rs
+++ src/bin/task_management.rs
@@ -221,7 +221,6 @@ impl TaskManagement {
 }
 
 fn handle_sigterm(shutdown: Arc<AtomicBool>) {
-    println!("HANDLE SIGTERM");
     let mut signals = Signals::new(&[SIGTERM]).expect("Failed to register SignalsInfo");
     for sig in signals.forever() {
         println!("RECEIVED SIGNAL {}", sig);
--- src/sigterm_handler/sigterm_handler.rs
+++ src/sigterm_handler/sigterm_handler.rs
@@ -3,7 +3,6 @@ use signal_hook::{consts::SIGTERM, iterator::Signals};
 use std::sync::Arc;
 
 pub fn handle_sigterm(shutdown: Arc<AtomicBool>) {
-    println!("HANDLE SIGTERM");
     let mut signals = Signals::new(&[SIGTERM]).expect("Failed to register SignalsInfo");
     for sig in signals.forever() {
         println!("RECEIVED SIGNAL {}", sig);
